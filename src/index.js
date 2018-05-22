0e177995595c7ef5242618295b672847ebc1d0fe
jongtelles <jongtelles@gmail.com>
2018-05-22 14:28:00 -0400

    updating webpack config and updating components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Hello from './hello';
 
 class App extends React.Component {
     render() {
         return (
             <div>
-        'Hello Jon!'
+                <Hello hello={'Hello, world! And the people of the world! I AM USING WEBPACK!!'} />
             </div>
         );
     }
